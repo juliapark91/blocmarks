51c6b9426f02587173b54cc30a19e64b3b9e597f
Conrad Taylor <conradwt@gmail.com>
2015-04-12 05:55:42 -0700

    Add IncomingComingController test.

---
+++ test/controllers/incoming_controller_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class IncomingControllerTest < ActionController::TestCase
+ 
+  def setup
+    @params = { 'sender': 'someone@example.com', 
+                'subject': 'top tech to watch in 2015', 
+                'body-plain': 'http://www.top-tech.com' }
+  end
+  
+  test 'should create a new bookmark' do
+    assert_difference( 'Bookmark.count', 1 ) do
+      post :create, @params
+    end
+  
+    assert_response :success
+  end
+  
+  test 'should create a new user if it does not exist' do
+    assert_difference( 'User.count', 1 ) do
+      post :create, @params
+    end
+
+    assert_response :success
+  end
+  
+  test 'should create a new topic if it does not exist' do
+    assert_difference( 'Topic.count', 1 ) do
+      post :create, @params
+    end
+
+    assert_response :success
+  end
+ 
+end
