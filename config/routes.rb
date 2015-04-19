6343952a6c8371c3bf465b81a7ff2c19f49748f2
Conrad Taylor <conradwt@gmail.com>
2015-04-19 00:52:05 -0700

    Fix: routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+
   resources :topics do
     resources :bookmarks, except: [:index]
   end
-
-  devise_for :users
+  
   get "welcome/index"
   post '/incoming', to: 'incoming#create'
   
