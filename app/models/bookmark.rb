47176538b5a7ac3d3af4dbcf28578122db9ed1af
Julia Park <juliapark91@gmail.com>
2015-04-22 21:06:37 -0700

    added thumbnail avatar images

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,14 +5,16 @@ class Bookmark < ActiveRecord::Base
 
 
 private 
-  
+
   def normalize_url
     self.url = 'http://' + url unless (url.empty? || url.match(/^https?:\/\//))
+    self.url = 'http://www.' + url unless (url.empty? || url.match(/www/))
   end  
 
   def set_image_source
-    object = LinkThumbnailer.generate( self.url )
-    self.image_source = object.images.first.src.to_s || "http://placehold.it/150x150"
+    #object = LinkThumbnailer.generate( self.url )
+    #self.image_source = object.images.first.src.to_s || "http://placehold.it/150x150"
+    self.image_source = Faker::Avatar.image("my-own-slug", "150x150")
   end
 
 
