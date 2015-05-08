ba02cb88fab2675fdf77b00533db55f2ebad49a3
Julia Park <juliapark91@gmail.com>
2015-05-08 14:42:26 -0700

    added user specific likes, without topics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   def show
     @topics = Topic.all
-    
+    @likes = current_user.likes
+
     # @user_bookmarks = current_user.bookmarks
-    # @user_likes = current_user.likes
+    @user_likes = current_user.likes
   end
 end
