71994490ba7ef337015ebf71d46482376cd9416f
Julia Park <juliapark91@gmail.com>
2015-05-11 19:59:29 -0700

    added like migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429152945) do
+ActiveRecord::Schema.define(version: 20150512025519) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "url"
